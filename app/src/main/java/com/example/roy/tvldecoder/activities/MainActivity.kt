f621c93425531c1ba748bcdb1d3860ffca51ea24
Roy Montoya <roymontoya89@gmail.com>
2018-02-21 09:17:50 -0600

    added buttons for examples

--- app/src/main/java/com/example/roy/tvldecoder/activities/MainActivity.kt
+++ app/src/main/java/com/example/roy/tvldecoder/activities/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import com.example.roy.tvldecoder.R
 import com.example.roy.tvldecoder.adapters.TlvAdapter
 import com.example.roy.tvldecoder.utils.TlvDecoder.Companion.parseString
@@ -19,12 +22,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val recyclerList: RecyclerView = findViewById(R.id.recyclerView)
+        val buttonLayout: LinearLayout = findViewById(R.id.layout_buttons)
 
         linearLayoutManager = LinearLayoutManager(this)
         recyclerList.layoutManager = linearLayoutManager
 
-        adapter = TlvAdapter(this, parseString(getString(R.string.second_example)))
-        recyclerList.adapter = adapter
+        var buttonExample1: Button = findViewById(R.id.button_first_example)
+        buttonExample1.setOnClickListener {
+            buttonLayout.visibility = View.GONE
+            adapter = TlvAdapter(this, parseString(getString(R.string.first_test)))
+            recyclerList.adapter = adapter
+        }
+
+        var buttonExample2: Button = findViewById(R.id.button_second_example)
+        buttonExample2.setOnClickListener {
+            buttonLayout.visibility = View.GONE
+            adapter = TlvAdapter(this, parseString(getString(R.string.second_example)))
+            recyclerList.adapter = adapter
+        }
 
     }
 
